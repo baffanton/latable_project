93b1d926a23cdecd04655ddf90f99ebe455659fa
Baiandin.AV <baff-anton@yandex.ru>
2025-01-21 19:39:40 +0500

    frontend fixes #2

--- frontend/src/shared/ui/Title/ui/Title.tsx
+++ frontend/src/shared/ui/Title/ui/Title.tsx
@@ -1,30 +1,17 @@
 import { FC, ReactNode } from "react";
 import { TitleFontTypes, TitleSizeTypes } from "../model/Title.types";
-import { Skeleton } from "@shared/ui/Skeleton";
 import cn from "classnames";
 import s from "./Title.module.scss";
 
 interface TitleProps {
   children: ReactNode;
-  isLoading?: boolean;
   size?: TitleSizeTypes;
   className?: string;
   font?: TitleFontTypes;
   onClick?: () => void;
 }
 
-const Title: FC<TitleProps> = ({
-  isLoading = false,
-  size = "sm",
-  className = "",
-  font = "raleway",
-  children,
-  onClick,
-}) => {
-  if (isLoading) {
-    return <Skeleton type="title" size={size} />;
-  }
-
+const Title: FC<TitleProps> = ({ size = "sm", className = "", font = "raleway", children, onClick }) => {
   return (
     <p
       className={cn(
